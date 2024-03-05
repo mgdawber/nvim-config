24fd31109cefb6975c04c33d64ee06971a8eaf71
Matthew Dawber <mgdawber@gmail.com>
2024-03-05 20:11:41 +0000

    chore: Remove unused plugins

--- lua/plugins/nvim_lint.lua
+++ lua/plugins/nvim_lint.lua
@@ -8,10 +8,6 @@ local M = {
         local lint = require("lint")
 
         lint.linters_by_ft = {
-            javascript = { "eslint_d" },
-            typescript = { "eslint_d" },
-            javascriptreact = { "eslint_d" },
-            typescriptreact = { "eslint_d" },
             ruby = { "rubocop" },
             erb = { "rubocop" },
         }
